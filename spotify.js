dd54a0d9a124d7d5db46c0d06e49ed1e42964fb2
kylefollansbee <101531370+kylefollansbee@users.noreply.github.com>
2022-03-13 22:26:22 -0400

    Add files via upload

---
+++ spotify.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spotify() {
+    return (
+        <iframe src="https://open.spotify.com/embed/playlist/6NWbZH0G29Ng4r4rWf9gZx" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+    )
+}
+
+export const displaySpotify = () => {
+    <Spotify> </Spotify>
+}
